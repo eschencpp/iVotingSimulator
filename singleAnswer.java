e912d03140e2da8237966ca5ef0c175ef907b047
eschencpp <eschen@cpp.edu>
2022-09-27 22:34:51 -0700

    added single answer question

--- singleAnswer.java
+++ singleAnswer.java
@@ -1,5 +1,18 @@
 public class singleAnswer extends Question {
 
+    //Answer banks
+    String[] trueFalse = {"True","False"};
+    String[] yesNo = {"Yes","No"};
+
+    String[] answerBank = {};
+
+    public void setAnswerBank(){
+        
+    }
+
+    public String[] getAnswerBank(){
+        return answerBank;
+    }
     
 }
 
