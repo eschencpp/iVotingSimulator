78a431738536ac357b5a92d7de6d87dda2c7886a
Eric Chen <eschen@cpp.edu>
2022-09-29 01:01:59 -0700

    formatted main method

--- SimulatorDriver.java
+++ SimulatorDriver.java
@@ -1,3 +1,4 @@
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
@@ -7,56 +8,68 @@ public class SimulatorDriver {
           //Initialize votingService
           votingService vService = new votingService();
           System.out.println("Welcome to IVoteService");
-          //Create multiple choice question 1 and set question,choices, and answer key
-          multipleChoice mc1 = new multipleChoice();
-          //Set Question Prompt
-          mc1.setQuestion("What is the answer");
-          System.out.println(mc1.getQuestion());
-          //Set answer bank
-          String[] mc1answerBank = {"1","2","3","4"};
-          mc1.setAnswerBank(mc1answerBank);
-          //Set answer key
-          ArrayList<String> mc1Key = new ArrayList<> (Arrays.asList("1","2"));
-          mc1.setAnswer(mc1Key);
 
-          //Create 10 students
+          //Create 6 students
           ArrayList<Student> studentArr = new ArrayList<Student>();
           while(studentArr.size() < 10){
                studentArr.add(new Student());
           }
+          System.out.printf("The current number of students in this session is: %d\n\n", studentArr.size());
 
-          //Generate random answers for Students
+          //Create multiple choice question 1 and set question,choices, and answer key
+          multipleChoice mc1 = new multipleChoice();
+          //Set Question Prompt
+          mc1.setQuestion("MC1. Which of the following are programming languages?");
+          System.out.println(mc1.getQuestion());
+          //Set answer bank
+          String[] mc1answerBank = {"A. Java","B. PHP","C. Python","D. Spanish"};
+          mc1.setAnswerBank(mc1answerBank);
+          System.out.println("The choices are: " + mc1.getAnswerBank());
+          //Set answer key
+          ArrayList<String> mc1Key = new ArrayList<> (Arrays.asList("A. Java","B. PHP","C. Python"));
+          mc1.setAnswer(mc1Key);
+
+          //Students send vote
           for(int i = 0; i < studentArr.size(); i++){
                vService.vote(studentArr.get(i), mc1, generateRandomAns(mc1.getAnswerBank()));
-               System.out.printf("StudentID: %s\n",studentArr.get(i).getUID());
+               System.out.printf("\nStudentID: %s\n",studentArr.get(i).getUID());
                System.out.println("Answer: " + studentArr.get(i).getAnswer());
           }
 
           //Output the results
+          System.out.println("\nVoting has ended. Here are the results:");
           vService.displayAnswers();
-          System.out.println(vService.gradeAnswers(studentArr, mc1));
+          System.out.println("The correct answer is: "+mc1Key);
+          System.out.printf("The number of students that answered correctly are: %d\n",vService.gradeAnswers(studentArr, mc1));
+          System.out.printf("%% %.2f of students got the answer correct.\n\n", ((float)vService.gradeAnswers(studentArr, mc1)/studentArr.size()) * 100);
           
           //Reset vote service for next question
           vService.reset(studentArr);
 
-
+          System.out.println("-----------------------------------------------------------------");
           //Single Answer Question
           singleAnswer sa1 = new singleAnswer();
-          String[] sa1answerBank = {"A","B","C","D","E","F","G"};
+          sa1.setQuestion("SA1. Which continent is Canada located in?");
+          System.out.println(sa1.getQuestion());
+          String[] sa1answerBank = {"A. North America","B. Asia","C. Africa","D. Europe"};
           sa1.setAnswerBank(sa1answerBank);
-          ArrayList<String> sa1Key = new ArrayList<> (Arrays.asList("A"));
+          System.out.println("The choices are: " + sa1.getAnswerBank());
+          ArrayList<String> sa1Key = new ArrayList<> (Arrays.asList("A. North America"));
           sa1.setAnswer(sa1Key);
 
           //Generate random answers for Students
           for(int i = 0; i < studentArr.size(); i++){
                vService.vote(studentArr.get(i), sa1, generateRandomAns(sa1.getAnswerBank()));
-               System.out.println(studentArr.get(i).getUID() + "\n" + studentArr.get(i).getAnswer());
+               System.out.printf("\nStudentID: %s\n",studentArr.get(i).getUID());
+               System.out.println("Answer: " + studentArr.get(i).getAnswer());
           }
 
           //Output the results
+          System.out.println("\nVoting has ended. Here are the results:");
           vService.displayAnswers();
-          System.out.println(vService.gradeAnswers(studentArr, sa1));
-          vService.reset(studentArr);
+          System.out.println("The correct answer is: "+sa1Key);
+          System.out.printf("The number of students that answered correctly are: %d\n",vService.gradeAnswers(studentArr, sa1));
+          System.out.printf("%% %.2f of students got the answer correct.\n\n", ((float)vService.gradeAnswers(studentArr, sa1)/studentArr.size()) * 100);
 
      }
 
--- votingService.java
+++ votingService.java
@@ -9,6 +9,9 @@ public class votingService {
 
     /**
     * Method to vote for multiple choice questions
+    * @param  student  the student that is voting
+    * @param  q the type of question being asked
+    * @param  answer the answer that the student is submitting
     */
     public void vote(Student student, multipleChoice q, ArrayList<String> answer){
         //Check if student has previously submitted answer
@@ -21,6 +24,10 @@ public class votingService {
 
     /**
     * Method to vote for single answer questions.
+    * Ensures that there is only one answer chosen.
+    * @param  student  the student that is voting
+    * @param  q the type of question being asked
+    * @param  answer the answer that the student is submitting
     */
     public void vote(Student student, singleAnswer q, ArrayList<String> answer){
         //Ensure multiple answers are not chosen
@@ -33,10 +40,10 @@ public class votingService {
         totalAnswers.addAll(student.getAnswer());
     }
 
-
+    //Counts the occurrence of each answer choice
     public static void displayAnswers()
     {
-        // hashmap to store the frequency of element
+        //hashmap to store the frequency of element
         Map<String, Integer> hm = new HashMap<String, Integer>();
         
         for (String i : totalAnswers) {
@@ -44,12 +51,19 @@ public class votingService {
             hm.put(i, (j == null) ? 1 : j + 1);
         }
  
-        // displaying the occurrence of elements in the arraylist
+        //displaying the occurrence of elements in the arraylist
         for (Map.Entry<String, Integer> val : hm.entrySet()) {
             System.out.println(val.getKey() +" : " + val.getValue());
         }
     }
 
+    /**
+    * Method counts the number of correct answers from all students
+    * The answers must match exactly to be counted as correct (no partial credit).
+    * @param  students  an array of Students in the vote session
+    * @param  q the current question being graded
+    * @return      the number of correct answers
+    */
     public static int gradeAnswers(ArrayList<Student> students, Question q){
         int totalCorrect = 0;
         ArrayList<String> studentAns;
@@ -65,6 +79,10 @@ public class votingService {
         return totalCorrect;
     }
 
+    /**
+    * Method erases all inputted data for Students
+    * @param  students  an array of Students in the vote session
+    */
     public static void reset(ArrayList<Student> students){
         totalAnswers.clear();
 
